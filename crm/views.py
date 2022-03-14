3b1f8ebb5eca242e36b46f234503c7b7209387f0
Max-OSDG <m.yakovlenko@mail.ru>
2022-03-14 16:29:07 +0300

    metod Get и новая форма

--- crm/views.py
+++ crm/views.py
@@ -1,4 +1,19 @@
 from django.shortcuts import render
+from .models import Order
 
 # Create your views here.
 
+def first_page(request):
+    object_list = Order.objects.all()
+    return render(request, './index.html', {'object_list': object_list})
+
+def thanks_page (request):
+    name = request.GET ['name']
+    nom = request.GET ['nomer']
+
+    el = Order()
+    el.order_name = name
+    el.order_phone = nom
+    el.save()
+
+    return render(request, './Thanks_page.html', {'name': name, 'nom': nom })
