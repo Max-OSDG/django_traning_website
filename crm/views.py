5440042435ea5fa7edac758d51e7fdc53f353b70
Max-OSDG <m.yakovlenko@mail.ru>
2022-03-15 18:26:37 +0300

    Конец урок 2. Начало урок 3 зарегистрировали приложение price

--- crm/views.py
+++ crm/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render
 from .models import Order
 from .forms import OrderForms
+from cms.models import CmsSlider
 
 # Create your views here.
 
 def first_page(request):
-    object_list = Order.objects.all()
-    form = OrderForms()
-    return render(request, './index.html', {'object_list': object_list, 'form': form})
+    slider_list = CmsSlider.objects.all()
+
+    return render(request, './index.html', {'slider_list': slider_list,
+                                            })
 
 def thanks_page (request):
     name = request.POST['name']
