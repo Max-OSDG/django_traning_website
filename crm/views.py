485926efafd555b5f3b607514683c76853868b56
Max-OSDG <m.yakovlenko@mail.ru>
2022-03-16 12:54:41 +0300

    Сосздание CRM приложения. Временная становка на создании в моделе татусов заявки

--- crm/views.py
+++ crm/views.py
@@ -11,13 +11,15 @@ def first_page(request):
     pc_1 = PriceCard.objects.get(pk=1)
     pc_2 = PriceCard.objects.get(pk=2)
     pc_3 = PriceCard.objects.get(pk=3)
+    form  = OrderForms()
 
     price_table = PriceTable.objects.all()
     dict_obj = {'slider_list': slider_list,
                 'pc_1': pc_1,
                 'pc_2': pc_2,
                 'pc_3': pc_3,
-                'price_table': price_table}
+                'price_table': price_table,
+                'form': form}
     return render(request, './index.html', dict_obj )
 
 def thanks_page (request):
